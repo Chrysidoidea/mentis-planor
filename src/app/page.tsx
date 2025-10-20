b21868883a0f5e1883656936d46d25cdcb9e86bb
Arthur Korolev <asuantodilae@protonmail.com>
2025-10-20 08:11:46 +0700

    fix: fix apperiance of main page, days, month, year, header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,23 @@
 
 import Calendar from "@/components/calendar";
 import CursorGlow from "@/effects/glowCursorEffect";
+import { dayCalculator } from "@/config/weekDays";
 
 export default function Home() {
-  // const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
-  // const [currentMonth, setCurrentMonth] = useState(new Date().getMonth());
-
   const currentMonth = new Date().getMonth();
   const currentYear = new Date().getFullYear();
-  const currentDay = new Date().getDay();
-  const currentDate = new Date().getDate()
 
-  // console.log(daysInMonth(currentMonth, currentYear));
   return (
     <div className="min-h-lvh w-full bg-gradient-to-t z-40 from-cyan-950 to-cyan-800 p-1 ">
-      <h1 className="text-center text-amber-400 z-50 p-2 font-bold text-3xl ">
+      <h1 className="text-center text-amber-400 z-50 p-2 font-bold text-4xl m-5">
         Mentis Planor
       </h1>
-      <span className="text-amber-500 text-center font-bold pl-3">
-        Year: {currentYear}
-      </span>
-      <span className="text-amber-500 text-center font-bold">
-        Month: {currentMonth}
-      </span>
-      <span>Day: {currentDate}</span>
+      <section className="text-amber-500 font-bold text-2xl grid grid-cols-3 place-content-center place-items-center">
+        <span>Year: {currentYear}</span>
+        <span className="text-3xl">{dayCalculator()}</span>
+        <span>Month: {currentMonth}</span>
+      </section>
+
       <div className="grid content-center text-white z-20 rounded-xs text-center">
         <CursorGlow></CursorGlow>
         <Calendar month={currentMonth} year={currentYear} />{" "}
--- src/config/weekDays.ts
+++ src/config/weekDays.ts
@@ -1,1 +1,30 @@
-export const daysInWeek: Array<string> = ["Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"];
+export const daysInWeek: Array<string> = [
+  "Mon",
+  "Tues",
+  "Wed",
+  "Thu",
+  "Fri",
+  "Sat",
+  "Sun",
+];
+
+export const dayCalculator = () => {
+  const currentDay = new Date().getDay();
+
+  switch (currentDay) {
+    case 1:
+      return "Monday";
+    case 2:
+      return "Tuesday";
+    case 3:
+      return "Wednesday";
+    case 4:
+      return "Thursday";
+    case 5:
+      return "Friday";
+    case 6:
+      return "Saturday";
+    case 7:
+      return "Sunday";
+  }
+};
